6b45d66cc3fc92cb7900a91b99c45ea571672b67
Wahid Ali <wahidali@Wahids-MacBook-Pro.local>
2020-04-02 19:57:28 +0200

    commit

---
+++ Example/Pods/Target Support Files/Pods-learncocoapods_Example/Pods-learncocoapods_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_learncocoapods_Example : NSObject
+@end
+@implementation PodsDummy_Pods_learncocoapods_Example
+@end
---
+++ Example/Pods/Target Support Files/learncocoapods/learncocoapods-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_learncocoapods : NSObject
+@end
+@implementation PodsDummy_learncocoapods
+@end
